7d57847f751ee736e1b3b6cebb73b4048a64d0d8
Xavi Lozano Segarra <xavils@Xavis-MacBook-Air.local>
2015-04-24 10:51:15 +0800

    some stuff

--- application.js
+++ application.js
@@ -26,7 +26,6 @@ $ (document).ready(function(){
 			var container = document.querySelector('#container');
 			var msnry = new Masonry( container, {
 			  // options
-			  columnWidth: 200,
 			  itemSelector: '.topic'
 			});
 
@@ -38,7 +37,6 @@ $ (document).ready(function(){
 			// var msnry = $container.data('masonry');
     }
 	};
-
 
 	$.ajax(get12Topics);
 
@@ -67,16 +65,8 @@ $ (document).ready(function(){
 				var container = document.querySelector('#container');
 				var msnry = new Masonry( container, {
 				  // options
-				  columnWidth: 200,
 				  itemSelector: '.topic'
 				});
-			   //    var $container = $('#container');
-				// // initialize
-				// $container.masonry({
-				//   itemSelector: '.topic'
-				// });
-				// var msnry = $container.data('masonry');
-				console.log(masonry)
 			}
 		});
 	});
@@ -106,38 +96,71 @@ $ (document).ready(function(){
   			var container = document.querySelector('#container');
 				var msnry = new Masonry( container, {
 				  // options
-				  columnWidth: 200,
 				  itemSelector: '.topic'
 				});
 			}
 		});
 	});
-	
+
+	$('#searchButton').on('click', function(event){
+		console.log("this function is being run");
+
+		event.preventDefault();
+
+		$.ajax({
+			type: 'GET',
+			url: 'http://localhost:8000/logs/search/' + $('.search').val(),
+			datatype: 'json',
+			success: function(response) {
+				topic = response;
+	      console.log(response);
+	      $('.content').text('');
+	      for (var i = 0; i < topic.length; i++) {
+	      	var message = topic[i].message;
+	      	var titles = topic[i].titles;
+	      	var user = topic[i].username;
+	      	$('.content').append('<div class="col-xs-12 col-sm-6 col-lg-4 topic">' +
+	      		'<div class="bld">' +
+	      		'<span class="singleTopic">' + titles[0] + '.' + '</span>' +
+	      		'<span class="singleTopic">' + titles[1] + '.' + '</span>' +
+	      		'<span class="singleTopic">' + titles[2] + '</span>' +
+	      		'</div><div class="singleUser">' + "@" + user +
+	      		'</div><div>' + message + '</div></div>');
+	      }
+
+  			var container = document.querySelector('#container');
+				var msnry = new Masonry( container, {
+				  // options
+				  itemSelector: '.topic'
+				});
+			}
+		});
+	});	
 	
 
-	// $.('#post').on('click', function(){
-	// 	type: 'POST',
- //    url: 'http://localhost:8000/topics',
- //    dataType: 'json',
- //    success: function(response){
- //      topic = response;
- //      console.log(response);
- //      for (var i = 0; i < topic.length; i++) {
- //      	var message = topic[i].message;
- //      	var titles = topic[i].titles;
- //      	$('.content').append('<div class="col-xs-12 col-sm-6 col-lg-4 topic">' +
- //      		'<div class="bld">' + titles[0] + titles[1] + titles[2] +
- //      		'</div><div>' + message + "</div></div>")
- //      }
+	$('#post').on('click', function(){
+		$.ajax({
+			type: 'POST',
+	    url: 'http://localhost:8000/topics',
+	    data: {
+	    	topic: {
+	    		titles: $('.titles').val().replace('#','').replace('#','').replace('#','').split(' '),
+	    		message: $('.message').val()
+	    	}
+	    },
+	    dataType: 'json',
+	    success: function(response){
+	    	console.log(response);
+      }
 
- //      var $container = $('#container');
-	// 		// initialize
-	// 		$container.masonry({
-	// 		  itemSelector: '.topic'
-	// 		});
-	// 		var msnry = $container.data('masonry');
- //    }
-	// })
+   //    var $container = $('#container');
+			// // initialize
+			// $container.masonry({
+			//   itemSelector: '.topic'
+			// });
+			// var msnry = $container.data('masonry');
+	  })
+	})
 
 	$('#signUpButton').on('click', function(){
 		$.ajax({
